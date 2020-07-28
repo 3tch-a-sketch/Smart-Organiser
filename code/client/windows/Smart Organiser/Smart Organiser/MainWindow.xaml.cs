a3205b49f769674d6454157f5fc97a53a575f374
3tch-a-sketch <sametchells@icloud.com>
2020-07-28 12:46:51 +0100

    added server and client

---
+++ code/client/windows/Smart Organiser/Smart Organiser/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Smart_Organiser
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    public partial class MainWindow : Window
+    {
+
+        private string mainDB = "localhost:27017"; //should be changed to match a mongoDB instance running on the server
+
+
+
+        public MainWindow()
+        {
+            GetDbRefresh(mainDB);
+            InitializeComponent();
+        }
+        private void GetDbRefresh(string db)
+        {
+            Console.WriteLine(db);
+        }
+        private void refreshDB_Click(object sender, RoutedEventArgs e)
+        {
+            GetDbRefresh(mainDB);
+        }
+
+        private void addToQueue_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void removeFromQueue_Click(object sender, RoutedEventArgs e)
+        {
+            if (Keyboard.Modifiers.HasFlag(ModifierKeys.Shift))
+            {
+                removeFromQueue_Click_All(sender, e);
+            }
+            else
+            {
+                MessageBox.Show("Standard Click");
+            }
+        }
+        private void removeFromQueue_Click_All(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Shift Click");
+        }
+    }
+}
---
+++ code/server/index.js
@@ -0,0 +1,56 @@
+﻿const express = require("express");
+const fs = require("fs");
+const app = express(); 
+const mongo = require('mongodb'); 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
+const dbName = 'smartOrganiser';
+const collection = 'items';
+
+// https://www.w3schools.com/nodejs/nodejs_mongodb_insert.asp
+//MongoClient.connect(url, function (err, db) {
+//    if (err) throw err;
+//    var dbo = db.db("mydb");
+//    var myobj = { name: "Company Inc", address: "Highway 37" };
+//    dbo.collection("customers").insertOne(myobj, function (err, res) {
+//        if (err) throw err;
+//        console.log("1 document inserted");
+//        db.close();
+//    });
+//}); 
+
+app.post("/api/test", (req, res, err) => {
+    res.send(200);
+});
+
+app.post("/api/list", (req, res, err) => {
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("smartOrganiser");
+        dbo.collection("items").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+            db.close();
+        });
+    }); 
+});
+
+app.post("/api/add/:name/:comment/:reference", (req, res, err) => {
+    req.params.comment = req.params.comment.replace("_"," ");
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("smartOrganiser");
+        var item = { name: req.params.name, comment: req.params.comment, reference: req.params.reference};
+        dbo.collection("items").insertOne(item, function (err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+    }); 
+    console.log(req.params.name+" || "+req.params.comment+" || "+req.params.reference);
+    res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("listening 3000..."));
